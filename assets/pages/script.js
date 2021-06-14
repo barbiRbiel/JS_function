4fba98377710acba0e4b67aa57a61694a120e96b
barbiRbiel <barbibiel@gmail.com>
2021-06-14 17:10:48 -0300

    Uploading files

---
+++ assets/pages/script.js
@@ -0,0 +1,9 @@
+// Idea del proyecto final: Juego_ Piedra, papel o tijera
+//Desafio:
+function solicitarNombre() {
+  let nombreIngresado = prompt("Ingresar nombre de usuario:")
+  alert(" Nombre Ingresado");
+  let contraseñaIngresada = prompt("Ingresar contraseña:")
+  alert("Contraseña Ingresada");
+}
+solicitarNombre();
